4805499f3b28905e2cceb8f9f215c31ee1e487f0
Berthold <berthold.roiser@gmail.com>
2015-11-11 10:51:07 +0100

    fixed GeoJsonObject Type

--- src/main/java/at/jku/cis/radar/geojson/GeoJsonObject.java
+++ src/main/java/at/jku/cis/radar/geojson/GeoJsonObject.java
@@ -7,11 +7,20 @@ import org.codehaus.jackson.annotate.JsonSubTypes.Type;
 import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.codehaus.jackson.annotate.JsonTypeInfo.Id;
 
+//@formatter:off
 @JsonTypeInfo(property = GeoJsonObject.TYPE, use = Id.NAME)
-@JsonSubTypes({ @Type(GeoJsonFeatureCollection.class), @Type(GeoJsonFeature.class) })
+@JsonSubTypes({ 
+    @Type(name = GeoJsonObject.FEATURE, value = GeoJsonFeature.class),
+    @Type(name = GeoJsonObject.FEATURE_EVOLUTION, value = GeoJsonFeatureEvolution.class),
+    @Type(name = GeoJsonObject.FEATURE_COLLECTION, value = GeoJsonFeatureCollection.class)})
+//@formatter:on
 public interface GeoJsonObject extends Serializable {
 
     static final String TYPE = "type";
+    static final String FEATURE = "Feature";
+    static final String FEATURE_EVOLUTION = "FeatureEvolution";
+    static final String FEATURE_COLLECTION = "FeatureCollection";
+
     static final String FEATURE_ID = "id";
     static final String COORDINATES = "coordinates";
     static final String GEOMETRIES = "geometries";
