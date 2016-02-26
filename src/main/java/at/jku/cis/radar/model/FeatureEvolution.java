dc7988acf3d5e1df059945e07f86545960271235
Markus <Markus.Weissenbek@gmx.at>
2016-02-26 12:27:14 +0100

    editOrder is now always increased by 1 from the maximum editOrder of the featureGroup

--- src/main/java/at/jku/cis/radar/model/FeatureEvolution.java
+++ src/main/java/at/jku/cis/radar/model/FeatureEvolution.java
@@ -20,7 +20,7 @@ import com.vividsolutions.jts.geom.Geometry;
 
 @Entity
 @NamedQueries({
-        @NamedQuery(name = FeatureEvolution.FIND_NEWEST_BY_FEATURE_GROUP, query = "SELECT fe FROM FeatureEvolution fe where fe.event.id = :eventId AND fe.featureGroup = :featureGroup ORDER By fe.date DESC"),
+        @NamedQuery(name = FeatureEvolution.FIND_NEWEST_BY_FEATURE_GROUP, query = "SELECT fe FROM FeatureEvolution fe where fe.event.id = :eventId AND fe.featureGroup = :featureGroup ORDER By fe.date, fe.editorder DESC"),
         @NamedQuery(name = FeatureEvolution.FIND_BETWEEN_BY_EVENT_AND_FEATURE_GROUP, query = "SELECT fe FROM FeatureEvolution fe WHERE fe.event.id = :eventId AND fe.featureGroup = :featureGroup AND fe.date BETWEEN :fromDate AND :toDate ORDER BY fe.date") })
 @NamedNativeQuery(name = FeatureEvolution.FIND_NEWEST_BY_EVENT, query = "SELECT fe.id, fe.date, fe.featuregroup, fe.geometry, fe.event_id, fe.editorder, fe.status FROM (SELECT featuregroup, MAX(date) as maxdate FROM featureevolution WHERE event_id = :eventId AND date BETWEEN :fromDate AND :toDate GROUP By featureGroup) r INNER JOIN public.featureevolution fe ON fe.featuregroup = r.featuregroup AND fe.date = r.maxdate", resultClass = FeatureEvolution.class)
 public class FeatureEvolution extends BaseEntity {
