344160d2acd4649e9931097f2ab4dc6f698cf8a6
Markus <Markus.Weissenbek@gmx.at>
2016-02-26 12:22:30 +0100

    saved features will now correctly loaded and displayed

--- src/main/java/at/jku/cis/radar/model/FeatureEvolution.java
+++ src/main/java/at/jku/cis/radar/model/FeatureEvolution.java
@@ -22,7 +22,7 @@ import com.vividsolutions.jts.geom.Geometry;
 @NamedQueries({
         @NamedQuery(name = FeatureEvolution.FIND_NEWEST_BY_FEATURE_GROUP, query = "SELECT fe FROM FeatureEvolution fe where fe.event.id = :eventId AND fe.featureGroup = :featureGroup ORDER By fe.date DESC"),
         @NamedQuery(name = FeatureEvolution.FIND_BETWEEN_BY_EVENT_AND_FEATURE_GROUP, query = "SELECT fe FROM FeatureEvolution fe WHERE fe.event.id = :eventId AND fe.featureGroup = :featureGroup AND fe.date BETWEEN :fromDate AND :toDate ORDER BY fe.date") })
-@NamedNativeQuery(name = FeatureEvolution.FIND_NEWEST_BY_EVENT, query = "SELECT fe.id, fe.date, fe.featuregroup, fe.geometry, fe.event_id FROM (SELECT featuregroup, MAX(date) as maxdate FROM featureevolution WHERE event_id = :eventId AND date BETWEEN :fromDate AND :toDate GROUP By featureGroup) r INNER JOIN public.featureevolution fe ON fe.featuregroup = r.featuregroup AND fe.date = r.maxdate", resultClass = FeatureEvolution.class)
+@NamedNativeQuery(name = FeatureEvolution.FIND_NEWEST_BY_EVENT, query = "SELECT fe.id, fe.date, fe.featuregroup, fe.geometry, fe.event_id, fe.editorder, fe.status FROM (SELECT featuregroup, MAX(date) as maxdate FROM featureevolution WHERE event_id = :eventId AND date BETWEEN :fromDate AND :toDate GROUP By featureGroup) r INNER JOIN public.featureevolution fe ON fe.featuregroup = r.featuregroup AND fe.date = r.maxdate", resultClass = FeatureEvolution.class)
 public class FeatureEvolution extends BaseEntity {
 
     public static final String FIND_NEWEST_BY_FEATURE_GROUP = "FeatureEvolution.findNewestByFeatureGroup";
